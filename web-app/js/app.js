187299a853542120a5d9b267e5655bff47a1966b
Ian De La Cruz <ridlc10@gmail.com>
2015-10-15 00:13:32 +0800

    Added icons JS - Sending Parse Data
    CSS
    - Reworked CSS

---
+++ web-app/js/app.js
@@ -0,0 +1,62 @@
+Parse.initialize("VHNmZxSO2pSrd2KjsjW6a8QUdJYZLFS6Il4WDrQM", "s36ClRKw9cXstmvT9Z2L6d5H9CaoUc5V1R9vQ2kP");
+
+var Schedule = Parse.Object.extend("Schedule");
+
+
+
+$("#create_schedule").submit(function(event){
+	event.preventDefault();
+
+	var destination = document.getElementById("destination").value,
+		initial = document.getElementById("initial").value,
+		etd = document.getElementById("etd").value,
+		eta = document.getElementById("eta").value,
+		company = document.getElementById("bus_company").value,
+		plate = document.getElementById("plate_number").value,
+		seats = document.getElementById("seats_available").value,
+		fare = document.getElementById("fare").value;
+
+		// console.log(typeof "plate");
+
+	var newSchedule = new Schedule();
+
+	newSchedule.save({
+		destination:destination,
+		startingTerminal: initial,
+		arrival: eta,
+		departure: etd,
+		busCompany: company,
+		busPlate: plate,
+		seatsAmount: seats,
+		ticketPrice: fare
+	}, {
+	  success: function(newSchedule) {
+	    // Execute any logic that should take place after the object is saved.
+	    alert('New object created with objectId: ' + newSchedule.id);
+	    console.log('New object created with objectId: ' + newSchedule.id);
+	  },
+	  error: function(newSchedule, error) {
+	    // Execute any logic that should take place if the save fails.
+	    // error is a Parse.Error with an error code and message.
+	    alert('Failed to create new object, with error code: ' + error.message);
+	  }
+	});
+	alert(destination);
+});
+
+var query = new Parse.Query(Schedule);
+var id;
+query.find({
+  success: function(results) {
+    console.log("Successfully retrieved " + results.length);
+    // Do something with the returned Parse.Object values
+    for (var i = 0; i < results.length; i++) { 
+      var object = results[i];
+      id = object.id;
+      console.log(object);
+    }
+  },
+  error: function(error) {
+    status.error("Error: " + error.code + " " + error.message);
+  }
+});
